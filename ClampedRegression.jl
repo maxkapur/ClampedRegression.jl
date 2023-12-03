fb5e64aa57f8f2727581e3dd7d26521bf4f19a0a
Max Kapur <maxkapur@gmail.com>
2023-12-03 09:53:43 -0500

    simplify notation, format

--- ClampedRegression.jl
+++ ClampedRegression.jl
@@ -3,9 +3,9 @@ using SCIP
 using Plots
 using Random
 
+const BIG_M = 100
 const CLAMP_LB = -0.5
 const CLAMP_UB = 0.5
-const BIG_M = 100
 const TOL = 1e-4
 const VARIANCE = 0.1
 
@@ -43,6 +43,7 @@ function solveregression(a::Matrix{Float64}, b::Vector{Float64}, use_clamping::B
         # bpred_clamp = clamp.(bpred_linear, CLAMP_LB, CLAMP_UB)
         @variable(model, clamphelper_lb[1:n], Bin)
         @variable(model, clamphelper_ub[1:n], Bin)
+        # note: We may not need both directions of the implication here
         @constraint(model, [i in 1:n], clamphelper_lb[i] => {bpred_linear[i] ≤ CLAMP_LB})
         @constraint(model, [i in 1:n], !clamphelper_lb[i] => {bpred_linear[i] ≥ CLAMP_LB})
         @constraint(model, [i in 1:n], clamphelper_ub[i] => {bpred_linear[i] ≥ CLAMP_UB})
@@ -71,36 +72,43 @@ function solveregression(a::Matrix{Float64}, b::Vector{Float64}, use_clamping::B
 
     optimize!(model)
 
-    if use_clamping
-        # Validate that clamping math worked as expected
-        for z in bpred
-            @assert CLAMP_LB - TOL ≤ value(z) ≤ CLAMP_UB + TOL
-        end
+    # Validate that clamping math worked as expected
+    use_clamping && for z in bpred
+        @assert CLAMP_LB - TOL ≤ value(z) ≤ CLAMP_UB + TOL
     end
     return (x=value.(x), bpred=value.(bpred))
 end
 
+
 """
     makedata(n, m)
 
-Make some fake data with `n` observations and `m` independent
-variables.
+Make some fake data with `n` observations and `m` independent variables. 
+Return a `NamedTuple` with fields `a`, `b`, and `x`, giving the independent
+variables, dependent variables, and ground-truth coefficients, respectively.
 """
 function makedata(n::Int, m::Int)
     a = randn(n, m)
     x = randexp(m)
     b = a * x + VARIANCE * randn(n)
     clamp!(b, CLAMP_LB, CLAMP_UB)
-    return a, b, x
+    return (; a, b, x)
 end
 
+
+"""
+    main()
+
+Solve a sample problem with a single independent variable, with and
+without clamping, and generate a plot comparing the results.
+"""
 function main()
-    a, b, _ = makedata(100, 1)
-    res_linear = solveregression(a, b, false)
-    res_clamp = solveregression(a, b, true)
+    data = makedata(100, 1)
+    res_linear = solveregression(data.a, data.b, false)
+    res_clamp = solveregression(data.a, data.b, true)
 
     pl = plot()
-    scatter!(pl, a[:], b, label="data")
+    scatter!(pl, data.a[:], data.b, label="data")
     plot!(pl, z -> z * res_linear.x[1], label="linear fit")
     plot!(pl, z -> clamp(z * res_clamp.x[1], CLAMP_LB, CLAMP_UB), label="clamped fit")
     pl
